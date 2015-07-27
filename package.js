b45adac4dbf4e9fe3edc1a9a2deee6d5e093e437
Gabriel Rubens <grsabreu@gmail.com>
2015-07-27 20:04:45 -0300

    Adding gbit:faker

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'gbit:fakefill',
-  version: '0.0.2',
+  version: '0.0.1',
   summary: 'Create a random fake data such as names, addresses, and phone numbers based in your SimpleSchema',
   git: 'https://github.com/girassolbit/meteor-fakefill.git',
   documentation: 'README.md'
@@ -15,19 +15,27 @@ Package.onUse(function(api){
   api.versionsFrom('METEOR@1.1.0.2');
   
   api.use('aldeed:simple-schema@1.3.3');
+  api.imply('aldeed:simple-schema');
+
   api.use('mongo@1.1.0');
+  api.imply('mongo');
+
   api.use('underscore@1.0.3');
+  api.imply('underscore');
+
+  api.use('gbit:faker@0.0.3');
+  api.imply('gbit:faker');
   
   api.addFiles('lib/fakefill.js', 'server');
   api.addFiles('lib/fakefill-util.js', 'server');
   api.export('Fakefill', 'server');
-  api.export('faker', 'server');
 });
 
 Package.onTest(function(api){
   api.use('tinytest');
   api.use('gbit:fakefill');
   api.use('aldeed:simple-schema');
+  api.use('gbit:faker');
   api.use('mongo');
   
   api.addFiles('tests/fakefill-tests.js', 'server'); 
